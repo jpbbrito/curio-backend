93fbf8541087bb92bfcc9d5ba02bc26271121a47
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-16 09:58:00 -0300

    removendo .split env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : process.env.NODE_ENV == 'development' ? '.env.dev' : '.env'
-});
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev'});
+console.log(process.env);
 const knex = require('knex');
 const configuration = require('./knexfile');
 const Database = require('./src/database/index');
--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,10 +1,15 @@
-const apiKeys = process.env.API_KEYS.split(',');
+//const apiKeys = process.env.API_KEYS.split(',');
 
 module.exports = {
+
   checkApiKey(req, res, next) {
+    /*
     if (apiKeys.includes(req.headers.api_key)) {
       return next();
     }
     return res.status(401).json({ error: 'API_KEY invalid.' });
+    */
+    return next();
   },
+
 };
