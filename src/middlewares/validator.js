a4ad06b4710b95c20393c758689844e94a905964
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-17 07:35:42 -0300

    corrigindo logs do checkApiKey

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -2,11 +2,13 @@ const apiKeys = process.env.API_KEYS.split(',');
 
 module.exports = {
 
-  checkApiKey(req, res, next) {
-
-    if (apiKeys.includes(req.headers.api_key)) {
+  checkApiKey(request, response, next) {
+    console.log('[checkApiKey] api_key: ',request.headers.api_key);
+    console.log('[checkApiKey] apiKeys[] tipo: ', typeof apiKeys);
+    if (apiKeys.includes(request.headers.api_key)) {
+      console.log('[checkApiKey] api_key valido');
       return next();
     }
-    return res.status(401).json({ error: 'API_KEY invalid.' });
+    return response.status(401).json({ error: 'API_KEY invalid.' });
   },
 };
