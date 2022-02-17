a2e0fe0a370290874a2c748b78865045a4478dd1
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-17 08:32:24 -0300

    corrigingo logs checkApiKey

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,9 +1,8 @@
 const apiKeys = process.env.API_KEYS.split(',');
 
 module.exports = {
-
   checkApiKey(request, response, next) {
-    console.log('[checkApiKey] api_key: ',request.headers.api_key);
+    console.log('[checkApiKey] headers: ', request.headers);
     console.log('[checkApiKey] apiKeys[] tipo: ', typeof apiKeys);
     if (apiKeys.includes(request.headers.api_key)) {
       console.log('[checkApiKey] api_key valido');
