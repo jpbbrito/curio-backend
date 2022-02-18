170ec9c3871c987c305625823a8d8a06ad2d6e98
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-18 07:40:17 -0300

    .buildpacks não funciona

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-
-const swaggerDocument = require('../openapi.json');
+const fs = require('fs');
+const apiDoc = require('./../openapi.json');
 
 const app = express();
 app.use(express.json());
-app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1/doc', swaggerUi.serve, swaggerUi.setup(apiDoc));
 app.use('/api/v1', require('./routes/index.js'));
 
 module.exports = app;
