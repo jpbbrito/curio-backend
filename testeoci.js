b7f7b4d5f550ebec0f1849255c68a0a1142dcfe7
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-22 10:37:26 -0300

    delete file

--- testeoci.js
+++
@@ -1,7 +0,0 @@
-const { common, database } = require('oci-sdk');
-
-const provider = new common.ConfigFileAuthenticationDetailsProvider();
-
-const tenancyId = 'ocid1.tenancy.oc1..aaaaaaaakv3ibsqxq2hymut6azcsyprkb3qryfk22hkahnsjm4pmnyuwwl2a';
-const compartmentId = 'ocid1.compartment.oc1..aaaaaaaanewdynmctrau4oh62k6kwjaaw57u6xkefxyyrfclrsir36jl5zga';
-const adminPassword = '#S3nh4..4dm1n';
