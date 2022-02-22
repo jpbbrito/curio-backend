8357de2290f2998f50833b0fdda3bdcd31d75793
João Paulo B Brito <teiu0@teiu.teiu>
2022-02-21 21:10:28 -0300

    TNS

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const knex = require('knex');
 const configuration = require('./knexfile');
 const Database = require('./src/database/index');
 const app = require('./src/server');
-const fs = require('fs');
 
 console.log('NODE_ENV: ', Object.keys(process.env));
 console.log('TNS_ADMIN: ', process.env.TNS_ADMIN);
-const tns = fs.readFileSync(`${process.env.TNS_ADMIN}/tnsnames.ora`, {encoding: 'utf8'});
-console.log('TNS_ADMIN file: ', tns);
+console.log('PWD: ', process.env.PWD);
+
+
 async function start(){
   await Database.createConnection(knex, configuration[process.env.NODE_ENV]);
   app.listen(process.env.PORT, () => {
