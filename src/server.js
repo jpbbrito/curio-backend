939f097ccdf5f406f07dafacc48027174b2b6225
jpbbrito <teiu0@teiu.teiu>
2022-06-26 17:51:25 -0300

    fix: limit body json para 5120kb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: "5120kb"}));
 app.use('/api/v1', require('./routes/index.js'));
 
 module.exports = app;
