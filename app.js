4d3b92e61e03a5797c62e97fbd6a4c5d82d565d3
jpbbrito <teiu0@teiu.teiu>
2022-10-04 20:22:45 -0300

    Configuração da imagem docker

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.test.env' : process.env.NODE_ENV == 'development' ? '.dev.env' : '.stag.env' });
-const knex = require('knex');
-const configuration = require('./knexfile');
-const Database = require('./database/index');
-const app = require('./src/server');
+require('dotenv').config({ path: process.env.NODE_ENV === 'local' ? '.local.env' : process.env.NODE_ENV === 'development' ? '.dev.env' : '.env' })
+const knex = require('knex')
+const configuration = require('./knexfile')
+const Database = require('./database/index')
+const app = require('./src/server')
 
-console.log('NODE_ENV: ', Object.keys(process.env));
-console.log('PWD:', process.env.PWD);
-
-async function start(){
-  await Database.createConnection(knex, configuration[process.env.NODE_ENV]);
-  app.listen(process.env.PORT, () => {
-      console.log('Server running on PORT 3000');
-  });
+console.log('NODE_ENV: ', Object.keys(process.env))
+console.log('PWD:', process.env.PWD)
+console.log('KNEX.CONFIGURATION', configuration)
+async function start () {
+  await Database.createConnection(knex, configuration[process.env.NODE_ENV])
+  app.listen(process.env.API_PORT, () => {
+    console.log('Server running on PORT 3000')
+  })
 }
 
-start();
+start()
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 
-if(!process.env.NODE_ENV) {
-  require('dotenv').config({ path: '.dev.env' });
+if (!process.env.NODE_ENV) {
+  require('dotenv').config({ path: process.env.NODE_ENV === 'local' ? '.local.env' : process.env.NODE_ENV === 'development' ? '.dev.env' : '.env' })
 }
 
 module.exports = {
@@ -9,46 +9,46 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user:  process.env.DB_USER,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './database/migrations/',
-    },
+      directory: './database/migrations/'
+    }
   },
-  staging: {
+  local: {
     client: process.env.DB_CLIENT,
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user:  process.env.DB_USER,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './database/migrations/',
-    },
-  }, 
+      directory: './database/migrations/'
+    }
+  },
   production: {
     client: process.env.DB_CLIENT,
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user:  process.env.DB_USER,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
     pool: {
       min: 2,
-      max: 10,
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './database/migrations/',
-    },
-  },
+      directory: './database/migrations/'
+    }
+  }
 
-};
+}
