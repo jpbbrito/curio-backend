c4bdf5f0e450fe1d1a753678d2b29f9428e96bfd
jpbbrito <teiu0@teiu.teiu>
2022-12-17 09:43:03 -0300

    novo campos na tabela problems

--- src/services/google-maps.js
+++ src/services/google-maps.js
@@ -3,9 +3,14 @@ import axios from 'axios'
 export async function getInfoByGeolocation (apiKey, latitude, longitude) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
 
-  const result = await axios({
-    method: 'GET',
-    url
-  })
-  return result
+  try {
+    const result = await axios({
+      method: 'GET',
+      url
+    })
+    console.log('[getInfoByGeolocation] result -', result)
+    return result
+  } catch (error) {
+    console.log('[getInfoByGeolocation] error -', error)
+  }
 }
