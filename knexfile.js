fb23a27daa936a2937a94c5aa8a7b6f0905f6eff
jpbrito <jpbrito@latam.stefanini.com>
2023-06-01 21:26:27 -0300

    adicionando ssl para postgres

--- knexfile.js
+++ knexfile.js
@@ -37,7 +37,8 @@ export default {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
+      ssl: true
     },
     pool: {
       min: 2,
