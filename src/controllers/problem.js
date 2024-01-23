dc3f7a7bf6cc998bd7c2dc2608293448193c78a4
jpbrito <jpbrito@latam.stefanini.com>
2024-01-23 07:03:43 -0300

    getCities redis erro

--- src/controllers/problem.js
+++ src/controllers/problem.js
@@ -267,7 +267,7 @@ export async function getByUUID (request, response) {
 }
 export async function cities (request, response) {
   const cache = await Redis.get('problems-cities')
-  if (cache) {
+  if (cache !== 'error_redis') {
     return response.json(cache)
   }
   const cities = await problemRepository.fetchCities()
